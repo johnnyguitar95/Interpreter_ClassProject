9014aef2628e0957124119645370ba02e53474b5
John Patrick Halloran <jhallor@mscs.mu.edu>
2016-09-13 23:54:36 -0500

    things should be correct

--- hw2.scm
+++ hw2.scm
@@ -2,4 +2,11 @@
 
 ;John Halloran and Jakob Horner
 
-
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (var (lambda (value) #t))
+   (val (lambda (value) #t))
+   (env environment?)
+  )
+)
