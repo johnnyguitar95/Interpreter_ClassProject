636ddae4e9e25fb7a4c94006ab7074fb6f104624
Jakob Horner <jakob.horner@marquette.edu>
2016-09-14 01:05:45 -0500

    added apply-env

--- hw2.scm
+++ hw2.scm
@@ -5,8 +5,26 @@
 (define-datatype environment environment?
   (empty-env)
   (extend-env
-   (var (lambda (value) #t))
-   (val (lambda (value) #t))
+   (var (not null?))
+   (val (not null?))
    (env environment?)
   )
 )
+
+(define apply-env
+  (lambda (env search-var)
+    (if (environment? env)
+        (cases environment env
+          (empty-env () 'error-empty-environment)
+          (extend-env (var val env)
+                      (if (eq? var search-var)
+                          val
+                          (apply-env env search-var)
+                      )
+          )
+        )
+        'error-not-environment
+    )
+  )
+)
+
