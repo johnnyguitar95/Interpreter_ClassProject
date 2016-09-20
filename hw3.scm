ff318641793cdcd21925ad2e3d51cf1e664eeb25
John Patrick Halloran <jhallor@mscs.mu.edu>
2016-09-19 23:03:34 -0500

    got stuff from the text book

--- hw3.scm
+++ hw3.scm
@@ -14,17 +14,35 @@
 
 (define parse-expression
     (lambda (datum)
-	
+	(cond
+          ((symbol? datum) (var-exp datum))
+          ((pair? datum)
+           (if (eqv? (car datum) 'lambda)
+               (lambda-exp
+                (car (cadr datum))
+                (parse-expression (caddr datum)))
+               (app-exp
+                (parse-expression (car datum))
+                (parse-expression (cadr datum)))))
+          (else (report-invalid-concrete-syntax datum)))
     )
 )
 
-;TODO for 9/20
+(define report-invalid-concrete-syntax
+  (lambda (datum)
+    (eopl:error 'datum "Invalid Syntax for datum")))
 
 (define unparse-lc-exp
     (lambda (ast)
-	(cons 'a ('b))
-    )
-)
+	(cases lc-exp exp
+          (var-exp (var) var)
+          (lambda-exp (bound-var body)
+             (list 'lambda (list bound-var)
+               (unparse-lc-exp body)))
+          (app-exp (rator rand)
+             (list
+              (unparse-lc-exp rator) (unparse-lc-exp rand)))
+)))
 
 (define occurs-free? 
     (lambda (lst)
