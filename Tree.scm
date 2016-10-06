69cfb83ab2348c0ea8da82c04d2c1ce68d2d5b30
John Halloran <john.p.halloran@marquette.edu>
2016-10-06 00:23:07 -0500

    updated through gui

--- Tree.scm
+++ Tree.scm
@@ -2,7 +2,7 @@
 
 (define-datatype tree tree?
   (tree-null)
-  (tree-node 
+  (tree-node
    (datum number?)
    (left tree?)
    (right tree?))
@@ -11,29 +11,28 @@
 (define tree-add
   (lambda (num t)
     (if (tree? t)
-	(cases tree t
-	       (tree-null () (tree-node num t t))
-	       (tree-node (datum left right) 
-			  (if (< num datum)
-			      (tree-node datum (tree-add num left) right)
-			      (tree-node datum left (tree-add num right))
-			      )
-			  ))
-	'error-not-tree)))
+        (cases tree t
+               (tree-null () (tree-node num t t))
+               (tree-node (datum left right)
+                          (if (< num datum)
+                              (tree-node datum (tree-add num left) right)
+                              (tree-node datum left (tree-add num right))
+                              )
+                          ))
+        'error-not-tree)))
 
 (define tree-search
   (lambda (num t)
     (if (tree? t)
-	(cases tree t
-	       (tree-null () #f)
-	       (tree-node (datum left right)
-			  (cond 
-			   ((not (number? num)) #f)
-			   ((eq? num datum) #t)
-			   ((< num datum) (tree-search num left))
-			   (else (tree-search num right))))
-	       )
-	'error-not-tree)))
+        (cases tree t
+               (tree-null () #f)
+               (tree-node (datum left right)
+                          (cond
+                           ((not (number? num)) #f)
+                           ((eq? num datum) #t)
+                           ((< num datum) (tree-search num left))
+                           (else (tree-search num right))))
+               )
+        'error-not-tree)))
 
 (provide tree tree-null tree-node tree? tree-add tree-search)
-  
